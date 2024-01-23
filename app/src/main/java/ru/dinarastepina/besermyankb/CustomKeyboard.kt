3b94b7cb1e9737f66a66b829c659cc48403eadc1
dinarastepina <setare497@gmail.com>
2024-01-23 04:47:57 -0500

    turned on autocaps if edittext is empty

--- app/src/main/java/ru/dinarastepina/besermyankb/CustomKeyboard.kt
+++ app/src/main/java/ru/dinarastepina/besermyankb/CustomKeyboard.kt
@@ -3,9 +3,9 @@ package ru.dinarastepina.besermyankb
 import android.content.Context
 import android.inputmethodservice.InputMethodService
 import android.os.IBinder
-import android.util.Log
 import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.ExtractedTextRequest
 import android.view.inputmethod.InputConnection
 import android.view.inputmethod.InputMethodManager
 import ru.dinarastepina.keyboardview.Keyboard
@@ -47,7 +47,13 @@ class BesermyanKeyboard : InputMethodService(), KeyboardView.OnKeyboardActionLis
     override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
         val inputConnection: InputConnection = currentInputConnection
         when (primaryCode) {
-            Keyboard.KEYCODE_DELETE -> inputConnection.deleteSurroundingText(1, 0)
+            Keyboard.KEYCODE_DELETE -> {
+                inputConnection.deleteSurroundingText(1, 0)
+                val currentText = inputConnection.getExtractedText(ExtractedTextRequest(), 0).text
+                if (currentText.isEmpty()) {
+                    switchKeyboard(R.xml.caps_pad)
+                }
+            }
             Keyboard.KEYCODE_DONE -> inputConnection.sendKeyEvent(
                 KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER)
             )
@@ -56,10 +62,15 @@ class BesermyanKeyboard : InputMethodService(), KeyboardView.OnKeyboardActionLis
             -8 -> switchKeyboard(R.xml.caps_pad)
             -10, -11 -> switchKeyboard(R.xml.main_pad)
             else -> {
-                inputConnection.commitText(primaryCode.toChar().toString(), 1)
-                if (firstTime) {
+                val inserted = primaryCode.toChar()
+                inputConnection.commitText(inserted.toString(), 1)
+                if (firstTime || inserted.isUpperCase()) {
                     switchKeyboard(R.xml.main_pad)
                     firstTime = false
+                }
+
+                if (inserted.toString().trimEnd().endsWith(".")) {
+                    switchKeyboard(R.xml.caps_pad)
                 }
             }
         }
